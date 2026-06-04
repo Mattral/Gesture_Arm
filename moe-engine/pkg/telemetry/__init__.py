f901799ffbeaba5823606c3cadd16aff64765b7f
Mattral <mattralminn@gmail.com>
2026-06-04 15:59:23 +0900

    reclean

--- moe-engine/pkg/telemetry/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Structured telemetry: JSONL + TensorBoard."""
-from pkg.telemetry.logger import StructuredLogger, StepRecord
-
-__all__ = ["StructuredLogger", "StepRecord"]
--- moe-engine/pkg/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities: MFU, config loader, environment introspection."""
-from pkg.utils.mfu import MFUAccountant, compute_moe_flops
-from pkg.utils.config import load_config
-
-__all__ = ["MFUAccountant", "compute_moe_flops", "load_config"]
